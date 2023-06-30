c61ba2ad4297d469d613b40f3dc8a9e31a9676c0
rmessett15 <rmessett15@gmail.com>
2023-06-30 12:47:19 -0500

    added new screenshots, project titles, descriptions, and links to the projects.js file, in turn updating the Portfolio.js file

--- src/projects.js
+++ src/projects.js
@@ -2,55 +2,55 @@ export const projects = [
   {
     projectTitle: "Dude Where's My Show",
     description:
-      "Lizards are a widespread group of squamate reptiles, with over6,000 species, ranging across all continents except Antarctica",
+      "A client-side web application for searching the streaming availability of movies and tv shows.",
     imageLink:
       "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
     liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
     gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
   },
   {
-    projectTitle: "Dude Where's My Show",
+    projectTitle: "Fusion Fables",
     description:
-      "Lizards are a widespread group of squamate reptiles, with over6,000 species, ranging across all continents except Antarctica",
+      "A full-stack web application established for writers to come together to harness the fusion of collaboration and community.",
     imageLink:
-      "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
-    liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
-    gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
+      "https://user-images.githubusercontent.com/120127903/250159501-c9b19908-7e37-480b-8126-3379d303a4ad.png",
+    liveLink: "http://www.fusionfables.com/",
+    gitHub: "https://github.com/rmessett15/Fusion-Fables",
   },
   {
-    projectTitle: "Dude Where's My Show",
+    projectTitle: "Tech Blog",
     description:
-      "Lizards are a widespread group of squamate reptiles, with over6,000 species, ranging across all continents except Antarctica",
+      "A full-stack web application where users can come create and interact with content related to tech.",
     imageLink:
-      "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
-    liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
-    gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
+      "https://user-images.githubusercontent.com/120127903/250159643-a51d0557-0f08-4826-a0f0-f8d802408584.png",
+    liveLink: "https://mvc-tech-blog15.herokuapp.com/",
+    gitHub: "https://github.com/rmessett15/MVC-Tech-Blog",
   },
   {
-    projectTitle: "Dude Where's My Show",
+    projectTitle: "Code Quiz",
     description:
-      "Lizards are a widespread group of squamate reptiles, with over6,000 species, ranging across all continents except Antarctica",
+      "A simple web-application where users can come test their knowledge about coding.",
     imageLink:
-      "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
-    liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
-    gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
+      "https://user-images.githubusercontent.com/120127903/250159663-da19d91f-765b-460f-ba19-811feb8b94d6.png",
+    liveLink: "https://rmessett15.github.io/Coding-Quiz/",
+    gitHub: "https://github.com/rmessett15/Coding-Quiz",
   },
   {
-    projectTitle: "Dude Where's My Show",
+    projectTitle: "Weather Dashboard",
     description:
-      "Lizards are a widespread group of squamate reptiles, with over6,000 species, ranging across all continents except Antarctica",
+      "Using third-party and server-side apis to bring clients live weather updates and future forecasts from anywhere in the world.",
     imageLink:
-      "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
-    liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
-    gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
+      "https://user-images.githubusercontent.com/120127903/250159688-6d04a5b7-fe26-4dae-9380-dfa29258e05a.png",
+    liveLink: "https://rmessett15.github.io/Weather-Dashboard/",
+    gitHub: "https://github.com/rmessett15/Weather-Dashboard",
   },
   {
-    projectTitle: "Dude Where's My Show",
+    projectTitle: "My First Website",
     description:
-      "Lizards are a widespread group of squamate reptiles, with over6,000 species, ranging across all continents except Antarctica",
+      "The first website I designed and deployed to the worldwide web.",
     imageLink:
-      "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
-    liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
-    gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
+      "https://user-images.githubusercontent.com/120127903/250159711-eb5907ee-46ca-4291-8a3d-89e8dd43df4f.png",
+    liveLink: "https://rmessett15.github.io/My-First-Website/",
+    gitHub: "https://github.com/rmessett15/My-First-Website",
   },
 ];
