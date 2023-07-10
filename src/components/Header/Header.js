3d59757907300410eafe36ec1eae7b87f78f553f
rmessett15 <rmessett15@gmail.com>
2023-07-10 13:45:34 -0500

    added padding to responsive nav tabs and justtified content right

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 
 export default function Header ({currentPage, setCurrentPage}) {
     return (
-      <div className="headerParent" style={{padding: '0px', paddingLeft: '43px'}}>
+      <div className="headerParent" style={{ padding: '0px 0px 10px 43px' }}>
         <p id="header-text">RYAN MESSETT</p>
         <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </div>
